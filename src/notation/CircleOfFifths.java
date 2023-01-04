9dbf4bf98ca8817e2f7c319a63d9c096bea912cd
JarredTD <jarred.desrosiers@yahoo.com>
2023-01-03 20:20:02 -0500

    CalcKeySig added

--- src/notation/CircleOfFifths.java
+++ src/notation/CircleOfFifths.java
@@ -1,29 +1,29 @@
 package notation;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CircleOfFifths {
+public class CircleOfFifths {	
+	
+	private static final int MOD = 12;
 
-	private static final Map<Integer, String> noteMapFlat = new HashMap<Integer, String>();
+	private static final ArrayList<String> fifths = new ArrayList<String>();
 	static {
-		noteMapFlat.put(0, "C");
-		noteMapFlat.put(1, "Db");
-		noteMapFlat.put(2, "D");
-		noteMapFlat.put(3, "Eb");
-		noteMapFlat.put(4, "E");
-		noteMapFlat.put(5, "F");
-		noteMapFlat.put(6, "Gb");
-		noteMapFlat.put(7, "G");
-		noteMapFlat.put(8, "Ab");
-		noteMapFlat.put(9, "A");
-		noteMapFlat.put(11, "B");
+		fifths.add("C");
+		fifths.add("G");
+		fifths.add("D");
+		fifths.add("A");
+		fifths.add("E");
+		fifths.add("B");
+		fifths.add("Gb");
+		fifths.add("Db");
+		fifths.add("Ab");
+		fifths.add("Eb");
+		fifths.add("Bb");
+		fifths.add("F");
 	}
 
-	public CircleOfFifths() {
-	}
-
-	public int calcSemitoneDistance(Note note1, Note note2) {
+	public static int calcSemitoneDistance(Note note1, Note note2) {
 		int distance = 0;
 
 		char note1Char = note1.getNoteLetter();
@@ -40,6 +40,35 @@ public class CircleOfFifths {
 
 		return distance;
 
+	}
+	
+	public static List<String> calcKey(Note n) {
+		char letter = n.getNoteLetter();
+		String noteLetter = (n.getAccidental() == -1 ) ? letter+"b" : letter+"";
+		boolean isFlat = (n.getAccidental() == -1 || letter == 'F') ? true : false;
+		final List<String> keySignature = new ArrayList<String>();
+		
+		int index = fifths.indexOf(noteLetter);
+		
+		if (!isFlat) {
+			for (int i = 0; i < index; i++) {
+				int idx = (i - 1);
+
+				idx = (idx < 0) ? idx + MOD : idx;
+				
+				keySignature.add(fifths.get(idx) + "#");
+			}
+		} else {
+			for (int i = 11; i >= index; i--) {
+				if (i == 6) {
+					keySignature.add(fifths.get(0));
+					continue;
+				}
+				keySignature.add(fifths.get(i - 1));
+			}
+		}
+
+		return keySignature;
 	}
 
 }
